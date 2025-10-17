8885c5d45494b9ead0966c0247c53ceb9313534d
manji5 <fatihenescoban5@gmail.com>
2025-10-17 15:54:34 +0300

    5.Ders

---
+++ Ders5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Lütfen bir şey yazın ve Enter'a tıklayın: ");
+            //string kullaniciGirisi = Console.ReadLine();
+            //Console.WriteLine($"Kullanıcıdan gelen giriş: {kullaniciGirisi}");
+
+            //Console.Write("Adınız nedir? ");
+            //string isim = Console.ReadLine();
+            //Console.WriteLine($"Hoş geldin, {isim}.");
+
+            Console.Write("Yaşınız kaç? ");
+            //string metinYas = Console.ReadLine();
+
+            int sayiYas = int.Parse(Console.ReadLine());
+            Console.WriteLine(sayiYas + 10);
+        }
+    }
+}
