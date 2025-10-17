b0f3090ab23659fb19b7f6813b19fb9c7d8ef96e
manji5 <fatihenescoban5@gmail.com>
2025-10-17 12:16:08 +0300

    2.Ders

---
+++ Ders2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //veritipi degiskenAdi = deger;
+
+            //int yas = 25;
+
+            //int ogrenciSayisi = 30;
+
+            //Console.WriteLine("Yaşınız: " + yas);
+            //Console.WriteLine("öğrenci Sayısı: " + ogrenciSayisi);
+            //Console.WriteLine($"Yaşınız: {yas}\nÖğrenci Sayısı: {ogrenciSayisi}");
+
+            double boy = 1.83;
+            float hiz = 90.5f;
+
+            decimal urunFiyati = 199.99m;
+            decimal bakiye = 1500.50m;
+            Console.WriteLine($"Ürün fiyatı: {urunFiyati}\nBakiyeniz: {bakiye}");
+        }
+    }
+}
