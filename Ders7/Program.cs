f13c98c84ac7bc431fd87f3e9c551e4264887dc9
manji5 <fatihenescoban5@gmail.com>
2025-10-22 00:43:16 +0300

    8.Ders

---
+++ Ders7/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Ders7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  ">" (Büyük mü?)
+             *  "<" (Küçük mü?)
+             *  ">=" (Büyük veya Eşit mi?)
+             *  "<=" (Küçük veya Eşit mi?)
+             */
+
+            #region Tekli Karşılaştırma
+            //int sayi1 = 10;
+            //int sayi2 = 5;
+
+            //bool sonuc = sayi1 > sayi2;
+            //Console.WriteLine("10, 5'ten büyük mü? " + sonuc);
+
+            //bool sonuc2 = sayi1 < sayi2;
+            //Console.WriteLine("10, 5'ten küçük mü? " + sonuc2);
+
+            //bool sonuc3 = sayi1 >= sayi2;
+            //Console.WriteLine("10, 5'ten büyük veya eşit mi? " + sonuc3);
+
+            //bool sonuc4 = sayi1 <= sayi2;
+            //Console.WriteLine("10, 5'ten küçük veya eşit mi? " + sonuc4);
+
+            //Console.WriteLine("\n\n\n\n\n");
+
+            //bool sonuc5 = sayi1 == sayi2;
+            //Console.WriteLine("10, 5'e eşit mi? " + sonuc5);
+
+            //bool sonuc6 = sayi1 != sayi2;
+            //Console.WriteLine("10, 5'e eşit değil mi? " + sonuc6);
+            #endregion
+
+            #region İkili Karşılaştırma
+            //int yas = 20;
+            //bool ehliyetVar = true;
+
+            //bool arabaKullananilirMi = (yas < 18) && (ehliyetVar == true);
+            //Console.WriteLine($"Araba kullanabilir mi? {arabaKullananilirMi}\n\n");
+
+            //bool arabaKullananilirMi2 = (yas < 18) || (ehliyetVar == true);
+            //Console.WriteLine($"Araba kullanabilir mi? {arabaKullananilirMi2}\n\n");
+            #endregion
+
+            #region Değil Operatörü
+
+            bool girisYapildiMi = false;
+
+            Console.WriteLine("Giriş yapıldı mı? " +  girisYapildiMi);
+            Console.WriteLine("Giriş yapıldı mı? " +  !girisYapildiMi);
+
+            #endregion
+        }
+    }
+}
---
+++ Ders8/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ders8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Sayısal İfadelerde Atama ve Bileşik Atama
+            //int skor = 100;
+            //Console.WriteLine($"Başlangıç Skoru: {skor}");
+
+            //skor = skor + 50;
+            //Console.WriteLine($"Yeni Skor: {skor}");
+
+            //int bakiye = 1000;
+            //Console.WriteLine($"Başlangıç Bakiyesi: {bakiye}");
+
+            //bakiye += 500; // bakiye = bakiye + 500;
+            //Console.WriteLine($"Arttırılmış Bakiye: {bakiye}");
+
+            //bakiye -= 200; // 1500 - 200 -- bakiye = bakiye - 200;
+            //Console.WriteLine($"Çıkarılmış Bakiye: {bakiye}");
+
+            //bakiye *= 2;
+            //Console.WriteLine($"Çarpılmış Bakiye: {bakiye}");
+
+            //bakiye /= 2;
+            //Console.WriteLine($"Bölünmüş Bakiye: {bakiye}");
+            #endregion
+
+            string mesaj = "Merhaba";
+            // Merhaba, Dünya!
+            mesaj += ", Dünya!";
+
+            Console.WriteLine(mesaj);
+        }
+    }
+}
