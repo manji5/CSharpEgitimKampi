341b7bb6df3d58f22f2131697039417f02d9a0a5
manji5 <fatihenescoban5@gmail.com>
2025-10-24 14:35:44 +0300

    11.Ders

---
+++ Ders11/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ders11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Deneme 1
+            //Console.Write("Notonuzu Giriniz: ");
+            //int not = int.Parse(Console.ReadLine());
+
+            #region if-else
+            //string durum;
+            //if (not >= 50)
+            //{
+            //    durum = "Geçti";
+            //}
+            //else
+            //{
+            //    durum = "Kaldı";
+            //}
+            #endregion
+
+            // (koşul) ? dogruysa : yanlışsa;
+
+            //string durum = (not >= 50) ? "Geçti" : "Kaldı";
+
+            //Console.WriteLine(durum);
+            #endregion
+
+            int sayi = 5;
+
+            string sonuc = (sayi % 2 == 0) ? "Sayı Çifttir." : "Sayı Tektir.";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
