d6c230a813722de872aaf1643fc9e4d4eb4d59dd
manji5 <fatihenescoban5@gmail.com>
2025-10-27 12:14:31 +0300

    12.Ders

---
+++ Ders12/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ders12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            for (başlatma; koşul; artırma)
+            {
+                // Koşul 'true' olduğu sürece bu blok tekrar tekrar çalışır.
+            }
+            */
+
+            for (int i = 0; i <= 23; i++)
+            {
+                for (int j = 0; j <= 59; j++)
+                {
+                    Console.WriteLine($"Saat: {i}:{j}");
+                }
+            }
+        }
+    }
+}
