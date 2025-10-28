fde0a911903d0913a062b8b4eb62d2ade3990859
manji5 <fatihenescoban5@gmail.com>
2025-10-28 15:05:13 +0300

    14.Ders

---
+++ Ders14/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ders14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine("Continue: " + i);
+            }
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Break: " + i);
+            }
+        }
+    }
+}
