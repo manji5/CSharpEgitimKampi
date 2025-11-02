141026d820bc74c8cb52dc847e5d7adb17e63039
manji5 <fatihenescoban5@gmail.com>
2025-11-02 13:01:29 +0300

    15.Ders

---
+++ Ders15/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ders15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // veriTipi[] diziAdi;
+
+            int[] not;
+            string[] isimler;
+
+            // diziAdi = new veriTipi[boyut]
+
+            //int[] notlar;
+            //notlar = new int[30];
+
+            int[] notlar = new int[3];
+
+            // boyut - 1 
+            notlar[0] = 70;
+            notlar[1] = 85;
+            notlar[2] = 90;
+
+            Console.WriteLine($"İlk öğrencinin notu: {notlar[0]}");
+            Console.WriteLine($"İkinci öğrencinin notu: {notlar[1]}");
+            Console.WriteLine($"Üçüncü öğrencinin notu: {notlar[2]}");
+            Console.WriteLine($"Dördüncü öğrencinin notu: {notlar[3]}");
+        }
+    }
+}
