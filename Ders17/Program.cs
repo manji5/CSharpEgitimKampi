8866bd3d62e55c1f7d16b70a83edd335848fe9e8
manji5 <fatihenescoban5@gmail.com>
2025-11-04 12:41:23 +0300

    17.Ders

---
+++ Ders17/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Ders17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region for
+
+            //string[,] bolgeler = {
+            //    { "İstanbul", "Bursa", "Edirne" },
+            //    { "Ankara", "Konya", "Kayseri" },
+            //    { "İzmir", "Aydın", "Manisa" }
+            //};
+
+            //for (int satir = 0; satir < 3; satir++)
+            //{
+            //    for (int sutun = 0; sutun < 3; sutun++)
+            //    {
+            //        Console.WriteLine(bolgeler[satir, sutun]);
+            //    }
+            //}
+
+            #endregion
+
+            /*
+             * foreach (veriTipi degiskenAdi in koleksiyon) 
+             * {
+             *      kodlar
+             * }
+             */
+
+            //int[] notlar = { 80, 90, 75, 100 };
+
+            //foreach (int not in notlar)
+            //{
+            //    Console.WriteLine(not);
+            //}
+
+            string[,] bolgeler = {
+                { "İstanbul", "Bursa", "Edirne" },
+                { "Ankara", "Konya", "Kayseri" },
+                { "İzmir", "Aydın", "Manisa" }
+            };
+
+            foreach (string sehirler in bolgeler)
+            {
+                Console.WriteLine(sehirler);
+            }
+        }
+    }
+}
