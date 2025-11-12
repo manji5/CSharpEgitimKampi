6a5e56c0ca4e35249da6d73dab638a060e898895
manji5 <fatihenescoban5@gmail.com>
2025-11-12 10:17:20 +0300

    23.Ders

---
+++ Ders23/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ders23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            
+            Console.WriteLine(Topla(5, 3));
+            Console.WriteLine(Topla(5, 3, 7));
+            Console.WriteLine(Topla(5.8, 3.3, 7.4));
+        }
+
+        static int Topla(int a, int b) { return a + b; }
+        static int Topla(int a, int b, int c) { return a + b + c; }
+        static double Topla(double a, double b, double c) { return a + b + c; }
+
+        static void BilgiVer(string isim, int yas)
+        {
+            Console.WriteLine($"{isim}, {yas} yaşında.");
+        }
+
+        static void BilgiVer(int yas, string isim)
+        {
+            Console.WriteLine($"{yas} yaşındaki {isim}!");
+        }
+    }
+}
