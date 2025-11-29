b0ec39a3739b1e412e0ff82456795a348a565e92
manji5 <fatihenescoban5@gmail.com>
2025-11-29 15:48:09 +0300

    9.Ünite

---
+++ Ders36_37/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders36_37
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ders 36
+            //#region Klasor Oluşturma
+            //string klasorYolu = @"D:\DenemeKlasoru";
+
+            //if (Directory.Exists(klasorYolu)) 
+            //{
+            //    Console.WriteLine("Bu klasör zaten var!");
+            //}
+            //else
+            //{
+            //    Directory.CreateDirectory(klasorYolu);
+            //    Console.WriteLine("Klasör başarıyla oluşturuldu.");
+            //}
+            //#endregion
+
+            //#region Dosya Islemleri
+
+            ////File.Exists(yol)
+            ////File.Copy(kaynak, hedef)
+            ////File.Move(kaynak, hedef)
+            ////File.Delete(yol)
+
+            //string dosyaYolu = @"D:\DenemeKlasoru\notlar.txt";
+            //string kopyaYolu = @"D:\DenemeKlasoru\yedek_notlar.txt";
+
+            //if (File.Exists(dosyaYolu))
+            //{
+            //    //kopyalama
+            //    //File.Copy(dosyaYolu, kopyaYolu);
+            //    Console.WriteLine("Dosya yedeklendi.");
+
+            //    //silme
+            //    File.Delete(dosyaYolu);
+            //    Console.WriteLine("Orjinal dosya silindi.");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("İşlem Yapılacak Dosya Bulunamadı");
+
+            //    //oluşturma
+            //    File.Create(dosyaYolu).Close();
+            //}
+
+            //#endregion
+            #endregion
+
+            #region Ders 37
+
+            string dosyaYolu = "notlarim.txt";
+
+            //Dosyaya yazma
+            //using (StreamWriter yazar = new StreamWriter(dosyaYolu, append: true))
+            //{
+            //    yazar.WriteLine("Bugün hava çok güzel.");
+            //    yazar.WriteLine("C# öğrenmek harika.");
+            //}
+
+            //Console.WriteLine("Dosyaya yazıldı.");
+
+            if (File.Exists(dosyaYolu))
+            {
+                using (StreamReader okuyucu = new StreamReader(dosyaYolu))
+                {
+                    string satir;
+
+                    while ((satir = okuyucu.ReadLine()) != null)
+                    {
+                        Console.WriteLine(satir);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Okunacak dosya bulunamadı");
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ DiaryApp/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kişisel Günlük Uygulamasına Hoş Geldiniz!");
+            string secim;
+
+            do
+            {
+                Console.WriteLine("\n--- MENÜ ---");
+                Console.WriteLine("1. Günlük Yaz");
+                Console.WriteLine("2. Günlük Oku");
+                Console.WriteLine("3. Günlüğü Sil (Sıfırla)");
+                Console.WriteLine("4. Çıkış");
+                Console.Write("Seçim: ");
+                secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        GunlukYaz();
+                        break;
+
+                    case "2":
+                        GunlukOku();
+                        break;
+
+                    case "3":
+                        GunlukSil();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Görüşmek üzere...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Geçersiz seçim!");
+                        break;
+                }
+
+            } while (secim != "4");
+        }
+
+        static void GunlukYaz()
+        {
+            string dosyaYolu = "gunluk.txt";
+
+            Console.WriteLine("\n--- Sevgili Günlük... (Yazmayı bitirmek için ENTER'a bas)");
+            string metin = Console.ReadLine();
+
+            using (StreamWriter yazar = new StreamWriter(dosyaYolu, append: true))
+            {
+                yazar.WriteLine($"[{DateTime.Now}]");
+                yazar.WriteLine(metin);
+                yazar.WriteLine("----------------------------------------------------------------------------------");
+            }
+
+            Console.WriteLine("Günlük başarıyla kaydedildi!");
+        }
+
+        static void GunlukOku()
+        {
+            string dosyaYolu = "gunluk.txt";
+
+            Console.WriteLine("\n--- Eski Anılar ---");
+            
+            if (File.Exists(dosyaYolu))
+            {
+                using (StreamReader okuyucu = new StreamReader(dosyaYolu))
+                {
+                    string icerik = okuyucu.ReadToEnd();
+                    Console.WriteLine(icerik);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Henüz hiç günlük yazılmamış. İlk yazan sen ol!");
+            }
+
+            Console.WriteLine("\n(Devam etmek için bir tuşa basın...)");
+            Console.ReadKey();
+        }
+
+        static void GunlukSil()
+        {
+            string dosyaYolu = "gunluk.txt";
+
+            if (File.Exists(dosyaYolu))
+            {
+                Console.WriteLine("Tüm günlüğü silmek istediğine emin misin? (E/H): ");
+                string cevap = Console.ReadLine().ToUpper();
+
+                if (cevap == "E")
+                {
+                    File.Delete(dosyaYolu);
+                    Console.WriteLine("Günlük silindi. Tertemiz bir sayfa!");
+                }
+                else
+                {
+                    Console.WriteLine("İşlem iptal edildi.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Silinecek bir günlük dosyası bulunamadı.");
+            }
+        }
+    }
+}
